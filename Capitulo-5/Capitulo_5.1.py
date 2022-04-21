e594167e8258aabe1bf09126881196e211139e95
Vega015 <34118685+Vega015@users.noreply.github.com>
2022-04-21 18:35:50 -0500

    Add files via upload

---
+++ Capitulo-5/Capitulo_5.1.py
@@ -0,0 +1,28 @@
+
+#Write a series of conditional test . Print a statement
+#describingeach each test and your predcition for the results 
+# of each test.
+
+fruta = 'manzana'
+print("Is fruta == 'banana'? I predict False")
+print(fruta == 'banana')
+
+car = 'subaru'
+print("\n Is car == audi? I predict false")
+print(car =="audi")
+
+name = "Alejandro"
+print("\n Is name == alejandro? I predict false")
+print(name == "alejandro")
+
+last_name = "Vega"
+print("\n Is last name == Vega I predict true")
+print(last_name == "Vega")
+
+color = "BluE"
+print("\n Is color == Blue I predict False")
+print( color == "Blue")
+
+team = "Cruz Azul"
+print("\n Is team == Cruz Azul? I predict True")
+print( team == "Cruz Azul")
